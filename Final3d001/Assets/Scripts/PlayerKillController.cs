e7b81be8e2ee3096d0f5a622d520c34520013025
RyanMcklain <piro27@gmail.com>
2023-02-18 01:21:19 +0200

    wip

--- Final3d001/Assets/Scripts/PlayerKillController.cs
+++ Final3d001/Assets/Scripts/PlayerKillController.cs
@@ -5,17 +5,39 @@ using UnityEngine;
 
 public class PlayerKillController : NetworkBehaviour
 {
-    int health = 100;
+    public int health = 100;
     int killCount = 0;
 
     void Start() { }
 
-    void Update() {
-
-        if (Input.GetKeyDown(KeyCode.Space))
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
         {
+            // raycast forward
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, 100))
+            {
+                print("Hit: " + hit.collider.gameObject.name);
+                if (hit.collider.gameObject.tag == "Player")
+                {
+                    PlayerKillController hitPlayer =
+                        hit.collider.gameObject.GetComponent<PlayerKillController>();
+                    hitPlayer.OnHit();
 
+                    if (hitPlayer.health <= 0)
+                    {
+                        OnKill();
+                    }
+                }
+            }
         }
+    }
+
+    public void OnHit()
+    {
+        health -= 10;
+        print("Player hit! Health: " + health);
     }
 
     void OnKill()
@@ -31,6 +53,7 @@ public class PlayerKillController : NetworkBehaviour
             health -= 10;
             if (health <= 0)
             {
+                health = 100;
                 GetComponent<NetworkObject>().Spawn();
             }
         }
